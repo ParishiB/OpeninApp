c9a6f11aea676007f42cf0689528c2a691ca46cf
PARISHI BHANGE <parishibhange@PARISHIs-MacBook-Pro.local>
2024-01-31 12:53:41 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,20 @@ import {
   Route,
   Routes,
   Navigate,
+  HashRouter,
 } from "react-router-dom";
 import Upload from "./Upload.jsx";
 import Signup from "./Signup.jsx";
 
 const App = () => {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/signup" />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/upload" element={<Upload />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 };
 
